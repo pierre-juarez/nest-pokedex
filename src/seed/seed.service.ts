32526dde702a541911064cc21c6d5f284e6806e6
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2025-06-28 02:16:09 -0500

    [feat] - Custom provider

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
-import { PokemonService } from 'src/pokemon/pokemon.service';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
-import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     // private readonly pokemonService: PokemonService
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
@@ -23,7 +20,7 @@ export class SeedService {
     // const insertPromisesArray: Promise<any>[] = [];
     const pokemonToInsert: { name: string; no: number }[] = [];
 
-    const { data } = await this.axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
