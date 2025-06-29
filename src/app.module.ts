06c0c16d245b6b2c58a2e35f02a97ea3f85a7680
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2025-06-29 01:02:10 -0500

    [feat] - Add deploy mongodb cloud (Railway)

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,9 @@ import { JoiValidationSchema } from './config/joi.validation';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(process.env.MONGODB ?? ''),
+    MongooseModule.forRoot(process.env.MONGODB ?? '', {
+      dbName: process.env.MONGO_DATABASE
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -4,4 +4,5 @@ export const JoiValidationSchema = Joi.object({
   MONGODB: Joi.required(),
   PORT: Joi.number().default(3000),
   DEFAULT_LIMIT: Joi.number().default(7),
+  MONGO_DATABASE: Joi.string().required(),
 });
